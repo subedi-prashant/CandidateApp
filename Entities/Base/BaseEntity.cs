822c7d8f389f91f040ba958e7b84ee90a5a18b78
subedi-prashant <70273082+subedi-prashant@users.noreply.github.com>
2024-05-25 09:59:23 +0545

    Add required models.

---
+++ Entities/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Entities.Base
+{
+    public class BaseEntity<TPrimaryKey>
+    {
+        [Key]
+        public TPrimaryKey Id { get; set; } = default!;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime? LastModifiedAt { get; set; }
+    }
+}
---
+++ Entities/Models/CandidateInfo.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    public class CandidateInfo : BaseEntity<int>
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string TimeIntervalToCall { get; set; }
+
+        public string LinkedInProfile { get; set; }
+
+        public string GitHubProfile { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+
+    }
+}
